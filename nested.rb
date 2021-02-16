8379160f0d03f591c981492421cfc66fc8989b5d
Jaryd Hermann <hermannjaryd@gmail.com>
2021-02-16 13:01:12 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -56,7 +56,8 @@ def dennis_ritchies_language
           :languages => ["C"]
         }
      }
-     programmer_hash[:dennis_ritchie][:languages].to_s
+     x = programmer_hash[:dennis_ritchie][:languages]
+     x.to_s
 
 end
 
